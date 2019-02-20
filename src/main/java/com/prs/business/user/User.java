a7f85e31854819dcd683821b96420a92fd7ff9f6
Eric Anspach <eric.j.anspach@gmail.com>
2019-02-19 23:31:51 -0500

    Spring Unit tests

--- src/main/java/com/prs/business/user/User.java
+++ src/main/java/com/prs/business/user/User.java
@@ -19,8 +19,24 @@ public class User {
     private String email;
     private boolean isReviewer;
     private boolean isAdmin;
+    
+    
 
-    public int getId() {
+    public User(String userName, String password, String firstName, String lastName, String phoneNumber,
+			String email, boolean isReviewer, boolean isAdmin) {
+		super();
+		
+		this.userName = userName;
+		this.password = password;
+		this.firstName = firstName;
+		this.lastName = lastName;
+		this.phoneNumber = phoneNumber;
+		this.email = email;
+		this.isReviewer = isReviewer;
+		this.isAdmin = isAdmin;
+	}
+
+	public int getId() {
         return id;
     }
 
